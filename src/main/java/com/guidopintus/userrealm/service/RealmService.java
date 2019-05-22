a8f937bc11cc90d8c29c70c4004b7810716a08ae
Guido Pintus <g.pintus@vargroup.it>
2019-05-23 00:46:39 +0200

    check for invalid realm name exception

--- src/main/java/com/guidopintus/userrealm/service/RealmService.java
+++ src/main/java/com/guidopintus/userrealm/service/RealmService.java
@@ -3,12 +3,14 @@ package com.guidopintus.userrealm.service;
 import com.guidopintus.userrealm.dao.RealmDAO;
 import com.guidopintus.userrealm.dto.AddRealmDTO;
 import com.guidopintus.userrealm.dto.RealmDTO;
+import com.guidopintus.userrealm.exception.InvalidRealmNameException;
 import com.guidopintus.userrealm.exception.RealmException;
 import com.guidopintus.userrealm.exception.RealmNotFoundException;
 import com.guidopintus.userrealm.model.RealmModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 @Service
 public class RealmService implements GetRealm, CreateRealm {
@@ -36,6 +38,11 @@ public class RealmService implements GetRealm, CreateRealm {
     public RealmDTO create(AddRealmDTO addRealmDTO) throws RealmException {
 
         String key = keyGen.getKey();
+
+        if(addRealmDTO.getName()==null || addRealmDTO.getName().isEmpty()){
+            throw new InvalidRealmNameException();
+        }
+
         RealmModel realm = new RealmModel(null, addRealmDTO.getName(), addRealmDTO.getDescription(), key);
 
         Integer id = realmDAO.add(realm);
