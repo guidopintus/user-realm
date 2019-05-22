f83d5ae2b2e3602d853447d9ace849555cb5e28c
Guido Pintus <g.pintus@vargroup.it>
2019-05-22 22:10:41 +0200

    implementaton of spring boot test suite

--- src/test/java/com/guidopintus/userrealm/UserrealmApplicationTests.java
+++ src/test/java/com/guidopintus/userrealm/UserrealmApplicationTests.java
@@ -2,15 +2,24 @@ package com.guidopintus.userrealm;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserrealmApplicationTests {
 
+	@Autowired
+	private ApplicationContext appContext;
+
 	@Test
 	public void contextLoads() {
+		assertNotNull(appContext);
 	}
 
 }
