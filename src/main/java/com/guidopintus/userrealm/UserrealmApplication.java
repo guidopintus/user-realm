1d8a0f869d9ea92569261ce0309ba1c10c0e39b6
Guido Pintus <g.pintus@vargroup.it>
2019-05-22 15:06:09 +0200

    first commit

---
+++ src/main/java/com/guidopintus/userrealm/UserrealmApplication.java
@@ -0,0 +1,13 @@
+package com.guidopintus.userrealm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserrealmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserrealmApplication.class, args);
+	}
+
+}
