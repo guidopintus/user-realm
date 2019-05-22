e1272312244ca6cd5b10b822128573a1e70b4fe9
Guido Pintus <g.pintus@vargroup.it>
2019-05-23 00:43:32 +0200

    change on GetRealm interface thrown exception

--- src/main/java/com/guidopintus/userrealm/service/GetRealm.java
+++ src/main/java/com/guidopintus/userrealm/service/GetRealm.java
@@ -1,12 +1,13 @@
 package com.guidopintus.userrealm.service;
 
 import com.guidopintus.userrealm.dto.RealmDTO;
+import com.guidopintus.userrealm.exception.RealmException;
 import com.guidopintus.userrealm.exception.RealmNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface GetRealm {
 
-    RealmDTO get(Integer id) throws RealmNotFoundException;
+    RealmDTO get(Integer id) throws RealmException;
 
 }
